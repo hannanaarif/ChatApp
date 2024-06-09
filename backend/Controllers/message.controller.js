5b132b1d684afbf5a48c210cb5b77d0010d3d8fd
Aarif Hannan <hannanaarif@gmail.com>
2024-06-09 19:08:49 +0530

    Script changes

--- backend/Controllers/message.controller.js
+++ backend/Controllers/message.controller.js
@@ -29,6 +29,12 @@ export const sendMessage=async(req,res)=>{
 
         await Promise.all([conversation.save(),newMessage.save()]);
 
+        const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
 
         return res.status(201).json(newMessage);
 
